bc2c4f3c64e3bb2983083366b75d67b14c0406a1
kaha440 <kaha440421@gmail.com>
2024-04-20 11:54:50 +0900

    Set up project

---
+++ src/main.rs
@@ -0,0 +1,77 @@
+mod command;
+mod package;
+mod version;
+
+use clap::{Parser, Subcommand};
+use colored::Colorize;
+use std::process::ExitCode;
+use thiserror::Error;
+
+#[derive(Debug, Parser)]
+#[clap(version, about)]
+struct App {
+    #[clap(subcommand)]
+    subcommand: SubCommand,
+}
+
+#[derive(Debug, Subcommand)]
+enum SubCommand {
+    #[clap(about = "Install a Node.js version")]
+    Install {
+        #[clap(required = true, help = "A version string")]
+        version: String,
+
+        #[clap(
+            short = 'p',
+            long = "reinstall-packages",
+            value_name = "FROM",
+            required = true,
+            help = "When installing, reinstall packages installed in <FROM>"
+        )]
+        packages_version: String,
+    },
+
+    #[clap(about = "Install the latest Node.js version")]
+    Update {
+        #[clap(
+            short = 'p',
+            long = "reinstall-packages",
+            value_name = "FROM",
+            help = "When installing, reinstall packages installed in <FROM>"
+        )]
+        packages_version: Option<String>,
+    },
+}
+
+fn main() -> ExitCode {
+    let result = match App::parse().subcommand {
+        SubCommand::Install {
+            version,
+            packages_version,
+        } => command::install(&version, &packages_version),
+        SubCommand::Update { packages_version } => command::update(packages_version.as_deref()),
+    };
+
+    match result {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(error) => {
+            eprintln!("{}", error.to_string().red());
+            ExitCode::FAILURE
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+enum AppError {
+    #[error("The requested version {version} is already installed.")]
+    AlreadyInstalled { version: String },
+
+    #[error("Can't find a Node version that matches `{version}` in remote.")]
+    CannotFindRemoteVersion { version: String },
+
+    #[error("Can't find an installed Node version that matches `{version}`")]
+    CannotFindVersion { version: String },
+
+    #[error("Can't reinstall global packages from {version}")]
+    CannotReinstallPackages { version: String },
+}
