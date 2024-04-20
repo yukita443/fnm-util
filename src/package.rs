f9846192ca37a8c61bd9ab504f4515d499052104
yukita443 <157708788+yukita443@users.noreply.github.com>
2024-04-20 12:23:40 +0900

    Add error handling for IO errors

--- src/package.rs
+++ src/package.rs
@@ -1,52 +1,52 @@
-use crate::version::{format_node_version, use_node};
+use crate::{
+    version::{format_node_version, use_node},
+    AppError,
+};
 use duct::cmd;
 use std::{path::Path, process::Command};
 
-pub fn install_packages(version: &str, packages_version: &str, packages: &[&str]) {
-    let current = cmd!("fnm", "current")
-        .read()
-        .expect("Failed to run `fnm current`");
+pub fn install_packages(
+    version: &str,
+    packages_version: &str,
+    packages: &[&str],
+) -> Result<(), AppError> {
+    let current = cmd!("fnm", "current").read()?;
 
-    use_node(version);
+    use_node(version)?;
 
     println!(
         "Reinstalling global packages from {}",
-        format_node_version(packages_version)
+        format_node_version(packages_version)?
     );
 
     Command::new("npm")
         .args(["install", "--global"])
         .args(packages)
-        .output()
-        .expect(&format!(
-            "Failed to run `npm install --global {}`",
-            packages.join(" ")
-        ));
+        .output()?;
 
-    use_node(&current);
+    use_node(&current)?;
+
+    Ok(())
 }
 
-pub fn packages_of(version: &str) -> Vec<String> {
-    let current = cmd!("fnm", "current")
-        .read()
-        .expect("Failed to run `fnm current`");
+pub fn packages_of(version: &str) -> Result<Vec<String>, AppError> {
+    let current = cmd!("fnm", "current").read()?;
 
-    use_node(version);
+    use_node(version)?;
 
-    let output = cmd!("npm", "list", "--global", "--parseable")
-        .read()
-        .expect("Failed to run `npm list --global --parseable`");
+    let output = cmd!("npm", "list", "--global", "--parseable").read()?;
     let list = output.lines().skip(1);
 
-    use_node(&current);
+    use_node(&current)?;
 
-    list.map(|s| {
-        Path::new(s)
-            .file_name()
-            .unwrap()
-            .to_os_string()
-            .into_string()
-            .unwrap()
-    })
-    .collect()
+    Ok(list
+        .map(|s| {
+            Path::new(s)
+                .file_name()
+                .unwrap()
+                .to_os_string()
+                .into_string()
+                .unwrap()
+        })
+        .collect())
 }
